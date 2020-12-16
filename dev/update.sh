d235bae0b463378f82f347d3ef1c2b0a8fcfd6e0
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-12-16 11:33:22 +0000

    Update script for external repos

--- dev/update.sh
+++ dev/update.sh
@@ -4,7 +4,26 @@
 cd /home/www/hpccertification/
 git pull -q
 git push -q
-cd data
+pushd data
 jekyll build
+popd
+
+for x in skill-data-events skill-data-material examination-tools  examination-questions ; do
+pushd $x
+git pull
+popd
+done
+
+# Updateables
+
+for x in examination-questions-staging  ; do
+pushd $x
+git add *
+git commit -m "Autocommit"
+git pull -q
+git push -q
+popd
+done
+
 ) > /dev/null
 
